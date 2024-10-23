5f3e4e50d56d3a483d92954f1a65900995643b66
Мария Архипенко <119293452+mariaArkhipenko123@users.noreply.github.com>
2024-10-23 21:53:45 +0300

    Update mArkhipenka_EnableDeactivateButton.j

--- mArkhipenka_EnableDeactivateButton.js
+++ mArkhipenka_EnableDeactivateButton.js
@@ -1,4 +1,4 @@
 function EnableDeactivateButton() {  
     const accountStatus = Xrm.Page.getAttribute("statecode").getValue();  
-    return accountStatus === 0; // Check that account status is active  
+    return accountStatus === 0;  
 }
